c48490d63d565cfdf1431fc4f59ec80de08bb4e0
Austin Smith <austinfosec@gmail.com>
2020-11-23 19:59:33 -0600

    initial commit

--- bin/moo-demo-project.ts
+++ bin/moo-demo-project.ts
@@ -4,4 +4,11 @@ import * as cdk from '@aws-cdk/core';
 import { MooDemoProjectStack } from '../lib/moo-demo-project-stack';
 
 const app = new cdk.App();
-new MooDemoProjectStack(app, 'MooDemoProjectStack');
+new MooDemoProjectStack(app, 'MooDemoProjectStack', {
+
+    // add aws region to app
+    env: {
+        region: 'us-east-1',
+    },
+
+});
